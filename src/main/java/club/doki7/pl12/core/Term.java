6068f15e194f63e9966850a184924c89abb5e064
chuigda <chuigda@doki7.club>
2026-01-24 15:33:37 +0800

    This might work, or might not work, idk

--- src/main/java/club/doki7/pl12/core/Term.java
+++ src/main/java/club/doki7/pl12/core/Term.java
@@ -3,92 +3,246 @@ package club.doki7.pl12.core;
 import club.doki7.pl12.syntax.Expr;
 import club.doki7.pl12.syntax.Token;
 import club.doki7.pl12.util.CommonUtil;
-import club.doki7.pl12.util.UV;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public sealed interface Term {
-    @NotNull UV<? extends Expr> exprUV();
-
-    default @NotNull Expr expr() {
-        return exprUV().e;
-    }
+    @NotNull Expr expr();
 
     sealed interface Checkable
             extends Term
-            permits Lam, Inferable {
-    }
+            permits Lam, Inferable
+    {}
 
     sealed interface Inferable
             extends Checkable
-            permits LamInf, Ann, Univ, Pi, Bound, Free, App, Meta {
-    }
+            permits LamInf, Ann, Univ, Pi, Bound, Free, App, Meta
+    {}
 
     record Lam(@NotNull Checkable body,
-               @NotNull UV<String> paramName,
-               @NotNull UV<Expr.Lam> exprUV)
+               @NotNull String paramName,
+               @NotNull Expr.Lam expr)
             implements Checkable
-    {}
+    {
+        @Override
+        public @NotNull String toString() {
+            StringBuilder sb = new StringBuilder();
+            sb.append("λ").append(paramName);
+
+            Checkable bodyIter = body;
+            while (bodyIter instanceof Lam lam) {
+                sb.append(" ").append(lam.paramName);
+                bodyIter = lam.body;
+            }
+
+            sb.append(". ").append(bodyIter);
+            return sb.toString();
+        }
+    }
 
     record LamInf(@NotNull Checkable paramType,
                   @NotNull Inferable body,
-                  @NotNull UV<String> paramName,
-                  @NotNull UV<Expr.Lam> exprUV)
+                  @NotNull String paramName,
+                  @NotNull Expr.Lam expr)
             implements Inferable
-    {}
+    {
+        @Override
+        public @NotNull String toString() {
+            List<String> paramNames = new ArrayList<>();
+            paramNames.add(paramName);
 
-    record Ann(@NotNull Checkable term, @NotNull Checkable ann, @NotNull UV<Expr.Ann> exprUV)
+            Inferable bodyIter = body;
+            while (bodyIter instanceof LamInf(Checkable paramType1,
+                                              Inferable body1,
+                                              String paramName1,
+                                              _)) {
+                if (!paramType1.equals(paramType)) {
+                    break;
+                }
+
+                paramNames.add(paramName1);
+                bodyIter = body1;
+            }
+
+            boolean needParen = paramType instanceof Pi
+                                || paramType instanceof LamInf
+                                || paramType instanceof Lam
+                                || paramType instanceof Ann
+                                || paramNames.size() > 1;
+
+            StringBuilder sb = new StringBuilder();
+            sb.append('λ');
+            if (needParen) {
+                sb.append('(');
+            }
+            for (String paramName : paramNames) {
+                sb.append(paramName).append(' ');
+            }
+            sb.append(": ").append(paramType);
+            if (needParen) {
+                sb.append(')');
+            }
+            sb.append(". ").append(bodyIter);
+            return sb.toString();
+        }
+    }
+
+    record Ann(@NotNull Checkable term, @NotNull Checkable ann, @NotNull Expr.Ann expr)
         implements Inferable
-    {}
+    {
+        @Override
+        public @NotNull String toString() {
+            if (term instanceof Univ
+                || term instanceof Free
+                || term instanceof Bound
+                || term instanceof Meta) {
+                return term + " : " + ann;
+            } else {
+                return "(" + term + ") : " + ann;
+            }
+        }
+    }
 
-    record Univ(@NotNull UV<Expr.Aster> exprUV) implements Inferable {}
+    record Univ(@NotNull Expr.Aster expr) implements Inferable {
+        @Override
+        public @NotNull String toString() {
+            return "*";
+        }
+    }
 
     record Pi(boolean implicit,
               @NotNull Checkable paramType,
               @NotNull Checkable bodyType,
-              @Nullable UV<String> paramName,
-              @NotNull UV<Expr.Pi> exprUV)
+              @Nullable String paramName,
+              @NotNull Expr.Pi expr)
         implements Inferable
-    {}
+    {
+        @Override
+        public @NotNull String toString() {
+            List<Pi> pies = new ArrayList<>();
+            pies.add(this);
 
-    record Bound(@NotNull Name name, @NotNull UV<Expr.Var> exprUV) implements Inferable {}
+            Checkable bodyTypeIter = bodyType;
+            while (bodyTypeIter instanceof Pi(boolean implicit1,
+                                      Checkable paramType1,
+                                      Checkable bodyType1,
+                                      String paramName1,
+                                      _)) {
+                if (implicit1 != implicit
+                    || !paramType1.equals(paramType)
+                    || (paramName1 == null) != (paramName == null)) {
+                    break;
+                }
+                pies.add((Pi) bodyTypeIter);
+                bodyTypeIter = bodyType1;
+            }
 
-    record Free(@NotNull Name name, @NotNull UV<Expr.Var> exprUV) implements Inferable {}
+            StringBuilder sb = new StringBuilder();
+            if (paramName == null) {
+                boolean needParen = paramType instanceof Pi
+                                    || paramType instanceof LamInf
+                                    || paramType instanceof Lam
+                                    || paramType instanceof Ann;
+
+                for (int i = 0; i < pies.size(); i++) {
+                    if (implicit) {
+                        sb.append('{');
+                    } else if (needParen) {
+                        sb.append('(');
+                    }
+                    sb.append(paramType);
+                    if (implicit) {
+                        sb.append("} -> ");
+                    } else if (needParen) {
+                        sb.append(") -> ");
+                    } else {
+                        sb.append(" -> ");
+                    }
+                }
+            } else {
+                sb.append(implicit ? "∀{" : "∀(");
+                for (Pi pi : pies) {
+                    sb.append(pi.paramName).append(' ');
+                }
+                sb.append(": ").append(paramType);
+                sb.append(implicit ? "}, " : "), ");
+            }
+            sb.append(bodyTypeIter);
+            return sb.toString();
+        }
+    }
+
+    record Bound(@NotNull Name name, @NotNull Expr.Var expr) implements Inferable {
+        @Override
+        public @NotNull String toString() {
+            return name.toString();
+        }
+    }
+
+    record Free(@NotNull Name name, @NotNull Expr.Var expr) implements Inferable {
+        @Override
+        public @NotNull String toString() {
+            return name.toString();
+        }
+    }
 
     record App(@NotNull Inferable func,
-               @NotNull List<@NotNull Checkable> args,
-               @NotNull UV<Expr.App> exprUV)
+               @NotNull Checkable arg,
+               @NotNull Expr.App expr)
         implements Inferable
-    {}
+    {
+        @Override
+        public @NotNull String toString() {
+            StringBuilder sb = new StringBuilder();
+            if (func instanceof Ann) {
+                sb.append('(').append(func).append(')');
+            } else {
+                sb.append(func);
+            }
+
+            sb.append(' ');
+
+            if (arg instanceof App
+                || arg instanceof Lam
+                || arg instanceof Pi
+                || arg instanceof Ann) {
+                sb.append('(').append(arg).append(')');
+            } else {
+                sb.append(arg);
+            }
+
+            return sb.toString();
+        }
+    }
 
     record Meta(int num,
-                @Nullable UV<Expr.Hole> hole,
-                @NotNull UV<Expr> intro,
-                @Nullable UV<Meta> introVar)
+                @Nullable Expr.Hole hole,
+                @NotNull Expr introCtx,
+                @Nullable Expr introVar)
             implements Inferable
     {
         @Override
         public @NotNull Expr expr() {
             if (hole != null) {
-                return hole.e;
+                return hole;
             }
-            return intro.e;
-        }
-
-        @Override
-        public @NotNull UV<? extends Expr> exprUV() {
-            return new UV<>(expr());
+            return introCtx;
         }
 
         @Override
         public @NotNull String toString() {
-            if (intro.e instanceof Expr.Var(Token name)) {
+            if (hole != null) {
+                return CommonUtil.subscriptNum("?ẖ", num);
+            }
+
+            if (introVar instanceof Expr.Var(Token name)) {
                 return CommonUtil.subscriptNum("?" + name.lexeme, num);
             }
 
-            return CommonUtil.subscriptNum("?", num);
+            return CommonUtil.subscriptNum("?α", num);
         }
     }
 }
