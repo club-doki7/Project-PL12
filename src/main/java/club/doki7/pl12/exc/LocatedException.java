f4fa773b65b0b36a671ffa9558ab26aef592596d
chuigda <chuigda@doki7.club>
2026-01-26 09:00:00 +0800

    咱们比赛爬山呀，比赛拔河呀，咱们比量比量。我经济上不如你们，我身体比你们棒。

--- src/main/java/club/doki7/pl12/exc/LocatedException.java
+++ src/main/java/club/doki7/pl12/exc/LocatedException.java
@@ -2,15 +2,21 @@ package club.doki7.pl12.exc;
 
 import club.doki7.pl12.syntax.Token;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class LocatedException extends Exception {
+    public record LocatedMessage(String message, Token location) {}
+
+    public final Token location;
+    public final String message;
+    public final List<LocatedMessage> trace = new ArrayList<>();
+
     public LocatedException(Token location, Class<?> clazz, String message) {
-        super(location.file + ":" + location.line + ": "
+        super(location.file + ":" + location.line + ":" + location.col + ": "
               + clazz.getSimpleName()
               + ": " + message);
         this.location = location;
         this.message = message;
     }
-
-    public final Token location;
-    public final String message;
 }
