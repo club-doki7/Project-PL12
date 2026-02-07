e6e673e2a27cb2cae1fa00b42bbf20c92e2511e3
Chuigda Whitegive <chuigda@doki7.club>
2026-02-07 21:26:20 +0800

    Postpone decisions?

--- src/main/java/club/doki7/pl12/core/Value.java
+++ src/main/java/club/doki7/pl12/core/Value.java
@@ -1,4 +1,11 @@
 package club.doki7.pl12.core;
 
+import club.doki7.pl12.util.ConsRevList;
+import org.jetbrains.annotations.NotNull;
+
 public interface Value {
+    interface Closure {
+        @NotNull ConsRevList<Value> localEnv();
+        @NotNull Term body();
+    }
 }
