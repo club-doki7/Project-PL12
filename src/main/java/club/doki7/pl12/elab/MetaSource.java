adee826a42d52b8a94eae4b9eb300c61048b188b
Cousin Ze <alex@ckx-ily.cn>
2026-02-09 10:12:49 +0800

    全忠实

--- src/main/java/club/doki7/pl12/elab/MetaSource.java
+++ src/main/java/club/doki7/pl12/elab/MetaSource.java
@@ -1,10 +1,19 @@
 package club.doki7.pl12.elab;
 
+import club.doki7.pl12.syntax.Command;
 import club.doki7.pl12.syntax.Expr;
 import org.jetbrains.annotations.NotNull;
 
 public sealed interface MetaSource {
-    record FunParamType(@NotNull Expr.Fun fun, int paramIndex) implements MetaSource {}
+    record DefParamType(@NotNull Command.Definition def,
+                        int paramGroupIndex,
+                        int paramIndex)
+        implements MetaSource {}
+
+    record FunParamType(@NotNull Expr.Fun fun,
+                        int paramGroupIndex,
+                        int paramIndex)
+        implements MetaSource {}
 
     record Hole(@NotNull Expr.Hole hole) implements MetaSource {}
 }
